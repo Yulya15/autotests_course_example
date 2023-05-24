10daad468c3c4bc2915873577cae6c1abd605463
uv.yasnikova <uv.yasnikova@tensor.ru>
2023-05-24 21:55:34 +0300

    новые изменения

--- Homework6/task2.py
+++ Homework6/task2.py
@@ -7,6 +7,10 @@
 def global_function():
   msg = 1
   def local_function():
+    """
+    Функция изменяет значение локальной переменной msg
+    :return: измененное значение локальной переменной
+    """
     nonlocal msg
     msg = 2
   local_function()
